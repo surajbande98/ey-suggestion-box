a302bd7c837f2a784e7972ee3a40c5bc3944a9a7
Suraj <suraj.bande98@gmail.com>
2023-10-10 09:42:41 +0530

    add debounce

--- suggestions-ui/src/App.tsx
+++ suggestions-ui/src/App.tsx
@@ -3,34 +3,29 @@ import AutoComplete from './components/autocomplete';
 import { Country } from './interfaces/Country.interface';
 
 import './App.css';
+import { debounce } from 'lodash';
+import { DEBOUNCE_TIME } from './constants';
 
 function App() {
-  const [searchedValue, setSearchedValue] = useState('');
   const [suggestions, setSuggestions] = useState<Country[]>([]);
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    const query = event.target.value;
-
+  async function search(query: string) {
     if (query !== '') {
-      setSearchedValue(query);
-      fetch(`http://localhost:5000/countries?query=${query}`, {
-        method: 'get',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-        .then((jsonResponse) => jsonResponse.json())
-        .then((result) => setSuggestions(result))
-        .catch(() => setSuggestions([]));
+      const response = await fetch(`http://localhost:5000/countries?query=${query}`);
+      const body = await response.json();
+      return body;
     } else {
-      setSearchedValue(query);
       setSuggestions([]);
     }
-  };
+  }
 
-  const onClearSearchQuery = () => {
-    setSearchedValue('');
-    setSuggestions([]);
+  const debouncedSearch = debounce(async (query) => {
+    setSuggestions(await search(query));
+  }, DEBOUNCE_TIME);
+
+  const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const query = event.target.value;
+    debouncedSearch(query);
   };
 
   return (
@@ -40,20 +35,14 @@ function App() {
         <div className="input-box">
           <input
             type="text"
-            value={searchedValue}
             name="search"
             placeholder="Search here"
             autoComplete="off"
             onChange={(event) => handleChange(event)}
           />
-          {searchedValue && (
-            <button className="erase-query" onClick={() => onClearSearchQuery()}>
-              x
-            </button>
-          )}
         </div>
         <div className="suggestion-box">
-          {suggestions.length > 0 && <AutoComplete suggestions={suggestions}></AutoComplete>}
+          {suggestions?.length > 0 && <AutoComplete suggestions={suggestions}></AutoComplete>}
         </div>
       </div>
     </>
